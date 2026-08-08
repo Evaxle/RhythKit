667d7ff910e4beeec0a1898e5f9b1c011caee80c
Evan <128929890+Evaxle@users.noreply.github.com>
2026-08-08 03:45:13 +0000

    build it again just fix the runtime

--- src/RhythKit/obj/Debug/net8.0-windows/RhythKit.AssemblyInfo.cs
+++ src/RhythKit/obj/Debug/net8.0-windows/RhythKit.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("RhythKit - The best external toolkit for Rhythia")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+515993c7396ad1bb460107ff56b6766c2f05b244")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f00802b5399ff08fef8813cff558bef64a07d80")]
 [assembly: System.Reflection.AssemblyProductAttribute("RhythKit")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RhythKit")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
