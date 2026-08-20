8f622e8a0f1ab17c5e8d963c1b0f0aec17d9ddec
Evan <128929890+Evaxle@users.noreply.github.com>
2026-08-19 22:22:11 -0400

    Delete src directory

--- src/RhythKit/Models/RhythiaProfile.cs
+++
@@ -1,8 +0,0 @@
-namespace RhythKit.Models;
-
-public class RhythiaProfile
-{
-    public int PlayerId { get; set; }
-    public string Username { get; set; } = "";
-    public string? AvatarUrl { get; set; }
-}
