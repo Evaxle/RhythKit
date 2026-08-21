a2bfd6e0dea974415e4cfc2341f587576f967c9e
Evan <128929890+Evaxle@users.noreply.github.com>
2026-08-21 18:32:00 -0400

    Support all Sound Space Plus agent game type aliases

--- src/RhythKit.Agent/AgentPorts.cs
+++ src/RhythKit.Agent/AgentPorts.cs
@@ -4,7 +4,7 @@ internal static class AgentPorts
 {
     public static int For(string gameType) => gameType switch
     {
-        "SspNightly" => 45873,
+        "SspNightly" or "SSP" or "Ssp" or "SoundSpacePlus" => 45873,
         "Vulnus" => 45874,
         _ => 45872
     };
